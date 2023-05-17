e3bc38e53516e8c05f5e5fd4cca0f53be831569f
Amarjot S <asangha52@my.bcit.ca>
2023-05-17 11:33:21 -0700

    mergeing dev into my branch

--- public/js/calorie.js
+++ public/js/calorie.js
@@ -19,26 +19,6 @@ $(document).ready(function() {
     console.log("val: " + circle.ariaValueNow);
     setProgress(circle.ariaValueNow);
   });
-
-  $("body").on("DOMContentLoaded", function() {
-    
-    var circle = document.querySelector("circle");
-    var radius = circle.r.baseVal.value;
-    var circumference = radius * 2 * Math.PI;
-  
-    circle.style.strokeDasharray = `${circumference} ${circumference}`;
-    circle.style.strokeDashoffset = `${circumference}`;
-  
-    function setProgress(percent) {
-      const offset = circumference - (percent / 100) * circumference;
-      circle.style.strokeDashoffset = offset;
-    }
-    var styles = window.getComputedStyle(circle);
-  
-    console.log(styles.getPropertyValue("animation"));
-    console.log("val: " + circle.ariaValueNow);
-    setProgress(circle.ariaValueNow);
-  });
 });
 
 
