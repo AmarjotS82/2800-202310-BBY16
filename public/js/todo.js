268249ea0e765d8acf14f98dc033e6c10a6fefdc
parinrava <pravanbakhsh@my.bcit.ca>
2023-05-17 16:09:38 -0700

    added addTodo and deleteCheckbox function

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,4 +1,61 @@
+/*This feature is inspired from chatgpt and made some modification 
+based on our app features and needs.
+*/
 
+//assigning classes into a const variable 
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
+//function to create a new todo item in the list
+function addTodo(event){
+    event.preventDefault(); //prevents default behaviour of form submission
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //adding to the local storage
+    saveLocalTodos(todo.value);
+
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = ' <i class="bi bi-check-circle"></i>'; //icon will show after user created list
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="bi bi-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+}
+
+function deleteCheckbox(e) {
+    const item = e.target;
+
+    if(item.classList[0] === "trash-btn"){
+        const todo = item.parentElement;
+        todo.classList.add("slide");
+
+        removeLocalTodos(todo);
+        todo.addEventlistener("transitionend", function(){
+            todo.remove();
+        });
+    }
+    if (item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
 
 
 
