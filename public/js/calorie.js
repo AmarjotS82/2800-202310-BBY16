ac65ae805f7829f874e3134e73ccca50907787e0
Amarjot S <asangha52@my.bcit.ca>
2023-05-19 10:52:02 -0700

    ADding process ring to card instead of midal and make ing it a gradient

--- public/js/calorie.js
+++ public/js/calorie.js
@@ -1,7 +1,6 @@
 // script.js
 $(document).ready(function() {
   console.log("reached calories.js");
-  $("body").on("click", "#amount", function() {
     console.log("clicked add");
     var circle = document.querySelector("circle");
     var radius = circle.r.baseVal.value;
@@ -9,7 +8,7 @@ $(document).ready(function() {
    console.log("radius:"+ radius);
     circle.style.strokeDasharray = `${circumference} ${circumference}`;
     circle.style.strokeDashoffset = `${circumference}`;
-    console.log("cir:"+ circumfrence);
+    console.log("cir:"+ circumference);
     function setProgress(percent) {
       console.log("%:"+ percent);
       const offset = circumference - (percent / 100) * circumference;
@@ -20,28 +19,28 @@ $(document).ready(function() {
     console.log(styles.getPropertyValue("animation"));
     console.log("val: " + circle.ariaValueNow);
     setProgress(circle.ariaValueNow);
-  });
+ 
 
 
-  $("body").on("click", "#goal", function() {
-    console.log("clicked goal");
-    var circle = document.querySelector("circle");
-    var radius = circle.r.baseVal.value;
-    var circumference = radius * 2 * Math.PI;
+  // $("body").on("click", "#goal", function() {
+  //   console.log("clicked goal");
+  //   var circle = document.querySelector("circle");
+  //   var radius = circle.r.baseVal.value;
+  //   var circumference = radius * 2 * Math.PI;
   
-    circle.style.strokeDasharray = `${circumference} ${circumference}`;
-    circle.style.strokeDashoffset = `${circumference}`;
+  //   circle.style.strokeDasharray = `${circumference} ${circumference}`;
+  //   circle.style.strokeDashoffset = `${circumference}`;
   
-    function setProgress(percent) {
-      const offset = circumference - (percent / 100) * circumference;
-      circle.style.strokeDashoffset = offset;
-    }
-    var styles = window.getComputedStyle(circle);
+  //   function setProgress(percent) {
+  //     const offset = circumference - (percent / 100) * circumference;
+  //     circle.style.strokeDashoffset = offset;
+  //   }
+  //   var styles = window.getComputedStyle(circle);
 
-    console.log(styles.getPropertyValue("animation"));
-    console.log("val: " + circle.ariaValueNow);
-    setProgress(circle.ariaValueNow);
-  });
+  //   console.log(styles.getPropertyValue("animation"));
+  //   console.log("val: " + circle.ariaValueNow);
+  //   setProgress(circle.ariaValueNow);
+  // });
 });
 
 
